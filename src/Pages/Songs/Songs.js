92c4b304f27f7fcffe4abfe16a80da905e66d460
swetha <swetha.gumpena0607@gmail.com>
2021-03-15 15:32:54 +0530

    add styles to songs page

--- src/Pages/Songs/Songs.js
+++ src/Pages/Songs/Songs.js
@@ -1,20 +1,26 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Card from '../../components/Card/Card';
+import styles from './Songs.module.scss';
 
 const Songs = ({ songs }) => (
   <>
-    <div>all songs</div>
-    {songs.map((song) => (
-      <Card
-        key={song.id}
-        id={song.id}
-        albumArtUrl={song.albumArtUrl}
-        songName={song.name}
-        genreName={song.genre.name}
-        artistName={song.artist.name}
-      />
-    ))}
+    <p className={styles.heading}>all songs</p>
+    <div className={styles.songsContainer}>
+      <div className={styles.card}>
+        {songs.map((song) => (
+          <Card
+            key={song.id}
+            id={song.id}
+            albumArtUrl={song.albumArtUrl}
+            songName={song.name}
+            genreName={song.genre.name}
+            artistName={song.artist.name}
+          />
+        ))}
+      </div>
+    </div>
+
   </>
 );
 
