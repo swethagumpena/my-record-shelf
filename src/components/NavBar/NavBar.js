88be00da8180028e44ae8ad114c10c4bf36b4c79
swetha <swetha.gumpena0607@gmail.com>
2021-03-15 14:56:04 +0530

    Add styling to nav bar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from './NavBar.module.scss';
 
 const NavBar = () => (
-  <div>My Record Shelf</div>
+  <div className={styles.navBar}>
+    <p>
+      My
+      {' '}
+      <span className={styles.bold}>Record</span>
+      {' '}
+      Shelf
+    </p>
+  </div>
 );
 
 export default NavBar;
